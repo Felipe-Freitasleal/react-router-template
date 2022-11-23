8209f35c5f61263514b2d7c25a01a1f32e553a22
Felipe-Freitasleal <felipeleal094@gmail.com>
2022-11-23 11:09:58 -0300

    Aula React Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
+import { gotToProfilePage, goToHomePage, gotToLoginPage } from "../Router/coordinato"
+import { useNavigate } from "react-router-dom";
+
+
 function Header() {
+
+  const navigate = useNavigate()
 
   return (
     <header>
-      <button >
+      <button onClick={() => goToHomePage(navigate)}>
         Ir para página inicial
       </button>
-      <button >
+      <button onClick={() => gotToProfilePage(navigate, `Felipe`)}>
         Ir para página de perfil
+      </button>
+      <button onClick={() => gotToLoginPage(navigate)}>
+        Ir para página de Login
       </button>
     </header>
   );
---
+++ src/pages/ErroPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function ErroPage () {
+
+    return(
+        <>
+        <p>Erro</p>
+        </>
+    )
+}
